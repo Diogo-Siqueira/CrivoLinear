d9c447ea97f28de9477177a61b0281fcdb53bff8
Diogo <diogpsiqueira@gmail.com>
2026-09-15 16:32:09 -0300

    Armazenando o menor fator de cada numero verificado de 2 até n

--- src/CrivoLinear.java
+++ src/CrivoLinear.java
@@ -1,41 +1,52 @@
 public class CrivoLinear {
 
     private int n;
-    private boolean[] composto;
+    private int[] menorFatorPrimo;
     private int[] primos;
 
     private int qtdPrimos;
     private boolean jaExecutado;
 
     public CrivoLinear(int n) {
-        if (n < 0) {
+        if (n < 1 ) {
             throw new IllegalArgumentException(
-                "não é possível realizar a busca de números primos, escreva um número maior ou igual a 0");
+                "é necessário que n seja maior que 1");
         }
+
         this.n = n;
-        this.composto = new boolean[n + 1];
+        this.menorFatorPrimo = new int[n + 1];
         this.primos = new int[n + 1];
         this.qtdPrimos = 0;
         this.jaExecutado = false;
     }
 
     public void executar() {
+        if (jaExecutado) {
+            throw new IllegalStateException(
+                "o crivo já foi executado");
+        }
+
         for (int i = 2; i <= n; i++) {
-            if (!composto[i]) {
+
+            if (menorFatorPrimo[i] == 0) {
+                menorFatorPrimo[i] = i;
+
                 primos[qtdPrimos] = i;
                 qtdPrimos++;
             }
 
             for (int j = 0; j < qtdPrimos; j++) {
+
+                if (primos[j] > menorFatorPrimo[i]) {
+                    break;
+                }
+
                 if (primos[j] > n / i) {
                     break;
                 }
                 int produto = i * primos[j];
-                composto[produto] = true;
 
-                if (i % primos[j] == 0) {
-                    break;
-                }
+                menorFatorPrimo[produto] = primos[j];
             }
         }
         jaExecutado = true;
@@ -52,10 +63,22 @@ public class CrivoLinear {
         verificarExecutado();
 
         int[] resultado = new int[qtdPrimos];
+
         for (int k = 0; k < qtdPrimos; k++) {
             resultado[k] = primos[k];
         }
+
         return resultado;
     }
 
+    public int pegarMenorFatorPrimo(int numero) {
+        verificarExecutado();
+
+        if (numero < 2 || numero > n) {
+            throw new IllegalArgumentException(
+                "o número deve estar entre 2 e " + n);
+        }
+
+        return menorFatorPrimo[numero];
+    }
 }
